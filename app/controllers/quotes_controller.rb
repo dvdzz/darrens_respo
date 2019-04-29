395db3cf2195f4a29a5a0bd079ca177a9595a82f
dvdzz <dvdzz76@icloud.com>
2019-04-29 21:56:09 +0000

    Added my bio and image

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -13,13 +13,22 @@ def index
       flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
   end
   redirect_to root_path
+
 end
+
+  def about
+
+
+
+
+  end
+
 
   private
 
   def quote_params
-    params.require(:quote).permit(:saying, :author)
+  params.require(:quote).permit(:saying, :author)
   end
-
-end 
+end
+ 
 
